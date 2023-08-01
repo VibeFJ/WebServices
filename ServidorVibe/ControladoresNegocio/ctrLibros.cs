ac3680518befbe19aa0614533efb34b0163f7471
unknown <raziel.rosfo.1904@gmail.com>
2023-08-01 10:59:50 -0600

    Subiendo actualización

---
+++ ServidorVibe/ControladoresNegocio/ctrLibros.cs
@@ -0,0 +1,94 @@
+﻿using ServidorVibe.Entidades;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ServidorVibe.ControladoresNegocio
+{
+    public class ctrLibros
+    {
+        private string administradorBD = ConfigurationManager.ConnectionStrings["NombreConexionBD"].ConnectionString;
+        public List<Libros> Obtener()
+        {
+            var respuesta = new List<Libros>();
+            try
+            {
+                using (var connection = new SqlConnection(administradorBD))
+                {
+                    connection.Open();
+
+                    var query = "SELECT * FROM Libros";
+                    var command = new SqlCommand(query, connection);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var serie = new Libros
+                            {
+                                LibroId = Convert.ToInt32(reader["LibroId"]),
+                                CodigoLibro = reader["CodigoLibro"].ToString(),
+                                Titulo = reader["Titulo"].ToString(),
+                                Caratula = reader["Caratula"].ToString(),
+                                Autor = reader["Autor"].ToString(),
+                                Tema = reader["Tema"].ToString(),
+                                AñoPublicacion = reader["AñoPublicacion"].ToString(),
+                                Editorial = reader["Editorial"].ToString(),
+                                Estatus = Convert.ToInt32(reader["Estatus"])
+                            };
+
+                            respuesta.Add(serie);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Add(new Libros{ CodigoLibro = "Error: " + ex.ToString()});
+            }
+            return respuesta;
+        }
+
+        public bool Actualizar(Libros objeto)
+        {
+            var respuesta = new List<Libros>();
+            try
+            {
+                using (var connection = new SqlConnection(administradorBD))
+                {
+                    connection.Open();
+
+                    var query = "SELECT * FROM Libros";
+                    var command = new SqlCommand(query, connection);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var serie = new Libros
+                            {
+                                LibroId = Convert.ToInt32(reader["LibroId"]),
+                                CodigoLibro = reader["CodigoLibro"].ToString(),
+                                Titulo = reader["Titulo"].ToString(),
+                                Caratula = reader["Caratula"].ToString(),
+                                Autor = reader["Autor"].ToString(),
+                                Tema = reader["Tema"].ToString(),
+                                AñoPublicacion = reader["AñoPublicacion"].ToString(),
+                                Editorial = reader["Editorial"].ToString(),
+                                Estatus = Convert.ToInt32(reader["Estatus"])
+                            };
+                            respuesta.Add(serie);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Add(new Libros { CodigoLibro = "Error: " + ex.ToString() });
+            }
+            return true;
+        }
+    }
+}
---
+++ ServidorVibe/Controllers/LibrosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+using ServidorVibe.ControladoresNegocio;
+using ServidorVibe.Entidades;
+
+namespace ServidorVibe.Controllers
+{
+    public class LibrosController : ApiController
+    {
+        [HttpPost]
+        [ActionName("Obtener")]
+        public List<Libros> Obtener()
+        {
+            var controlador = new ctrLibros();
+            var respuesta = controlador.Obtener();
+            return respuesta;
+        }
+
+        [HttpPost]
+        [ActionName("Actualizar")]
+        public bool Actualizar(Libros objeto)
+        {
+            var controlador = new ctrLibros();
+            var respuesta = controlador.Actualizar(objeto);
+            return respuesta;
+        }
+    }
+} 
